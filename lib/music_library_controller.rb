d6db9d82c0b946b83afc4ddaf1472bf61b371aaf
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-06 03:38:08 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -101,8 +101,7 @@ class MusicLibraryController
         song = Song.find_by_name(input)
           if song 
             puts "Playing #{song.name} by #{song.artist.name}"
-          binding.pry
-      end 
+        end 
     end
     
 
