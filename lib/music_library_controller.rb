f322e2fe85f6b2163e2442a7fdfc15cc6a9a820b
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-06 03:13:36 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -100,7 +100,6 @@ class MusicLibraryController
       input = gets.strip
       if song = Song.find_by_name(input)
         puts "Playing #{song.name} by #{song.artist.name}"
-        
     end
     
 
