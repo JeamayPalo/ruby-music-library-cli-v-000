b1a47c54e95352765c186b32a77a3ba71b386685
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-06 02:35:54 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -89,7 +89,7 @@ class MusicLibraryController
       input = gets.strip
       if genre = Genre.find_by_name(input)
           genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, number|
-          puts "#{number}. #{song.name} - #{song.artist.name}"
+          puts "#{number}. #{song.artist.name} - #{song.name}"
         end 
       end 
     end
