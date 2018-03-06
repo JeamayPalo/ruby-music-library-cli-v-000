fbee2343e8f4d29032e8e8f6b9e682ba992e9ffd
Jeamay Palo <jeamay.palo@gmail.com>
2018-03-06 00:57:20 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -77,7 +77,7 @@ class MusicLibraryController
     def list_songs_by_artist
       puts "Please enter the name of an artist:"
         input = gets.strip
-        artist = Artist.find_by_name(input)
+        if artist = Artist.find_by_name(input)
         binding.pry 
         array.each do |song|
           if song[0] == artist.name
